dcc41a75e41669a93203cccba655ec11e7bc7c56
Dmytro Teren <leo20091986@gmail.com>
2025-03-06 19:40:49 +0200

    mode task1

--- js/task-1.js
+++ js/task-1.js
@@ -6,10 +6,10 @@ console.log(`Number of categories: ${ulCategories.children.length}`);
 const itemsElement = document.querySelectorAll(".item");
 
 const getCategoriesValue = (items) => {
-    for (const item of items) {
-        console.log(`Category: ${item.firstElementChild.textContent}`);
-        console.log(`Elements: ${item.lastElementChild.children.length}`);
-    }
+    Array.from(items).forEach((element) => {
+        console.log(`Category: ${element.firstElementChild.textContent}`);
+        console.log(`Elements: ${element.lastElementChild.children.length}`);
+    });
 };
 
 getCategoriesValue(itemsElement);
