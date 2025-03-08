ca36ac4ad00103650f2dc3359c9a52acfea42a7c
Dmytro Teren <leo20091986@gmail.com>
2025-03-08 15:05:08 +0200

    solved task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const colorText = document.querySelector(".color");
+const btnColor = document.querySelector(".change-color");
+
+btnColor.addEventListener("click", changeColorBody);
+
+function changeColorBody(e) {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorText.textContent = color;
+}
