72a30a36b8397c377bea846c75105db65676e521
Dmytro Teren <leo20091986@gmail.com>
2025-03-08 13:43:02 +0200

    solved task3

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+"use strict"
 
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault()
+    const email = e.target.elements.email;
+    const password = e.target.elements.password;
+    const user = {};
+    if (email.value === "" || password.value === "") {
+        alert('All form fields must be filled in');
+        return;
+    } else {
+        user.email = email.value.trim().toLowerCase();
+        user.password = password.value.trim();
+    }
+    e.target.reset();
+    console.log(user);
+}
