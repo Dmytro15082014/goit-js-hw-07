3524698dbebf39a17c91dafc6e431c2b2e7e13ed
Dmytro Teren <leo20091986@gmail.com>
2025-03-08 12:17:29 +0200

    mode task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,13 @@
 "use strict"
+const inputEl = document.querySelector("#name-input");
+const outputEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", handleName);
+
+function handleName(event) {
+    if (event.target.value === "" || event.target.value === " ") {
+        outputEl.textContent = "Anonymous";
+    } else {
+        outputEl.textContent = event.target.value.trim();
+    }
+}
